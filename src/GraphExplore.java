ffcd2ad0f2a751a9c0b669396f89175d918451b5
yamiacat <yamiacat@gmail.com>
2017-09-23 18:25:15 +0100

    Position nodes

--- src/GraphExplore.java
+++ src/GraphExplore.java
@@ -14,6 +14,7 @@
 
     public class GraphExplore {
         public static void main(String args[]) {
+            System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
             new GraphExplore();
         }
 
@@ -23,24 +24,15 @@
             graph.addAttribute("ui.stylesheet", styleSheet);
             graph.setAutoCreate(true);
             graph.setStrict(false);
-            graph.display();
-
+            graph.display(false);
+            
+            //DEFINE RELATIONSHIPS
             graph.addEdge("CityAston", "City", "Aston");
-            
-            Node city = graph.getNode("City");
-            city.setAttribute("name", "City");
-               
-            
             graph.addEdge("AstonSmallheath", "Aston", "Smallheath");
-            
-            
             graph.addEdge("SmallheathCity", "Smallheath", "City");
-            
-            
             graph.addEdge("CityFiveways", "City", "Fiveways");
             graph.addEdge("FivewaysSmallheath", "Fiveways", "Smallheath");
             graph.addEdge("AstonPerrybarr", "Aston", "Perrybarr");
-            
             graph.addEdge("PerrybarrCity", "Perrybarr", "City");
             graph.addEdge("PerrybarrWalsall", "Perrybarr", "Walsall");
             graph.addEdge("PerrybarrErdington", "Perrybarr", "Erdington");
@@ -53,7 +45,46 @@
             graph.addEdge("AcocksgreenSelloak", "Acocksgreen", "Sellyoak");
             graph.addEdge("RedditchSelloak", "Redditch", "Sellyoak");
 
-                       
+            
+            //DEFINE POSITIONS
+            
+            Node city = graph.getNode("City");
+            city.setAttribute("xy", 0,0);
+               
+            Node aston = graph.getNode("Aston");
+            aston.setAttribute("xy", 1, 1);
+            
+            Node fiveways = graph.getNode("Fiveways");
+            fiveways.setAttribute("xy", -1, -1);
+            
+            Node smallheath = graph.getNode("Smallheath");
+            smallheath.setAttribute("xy", 1, -1);
+            
+            Node acocksgreen = graph.getNode("Acocksgreen");
+            acocksgreen.setAttribute("xy", 2, -2);
+            
+            Node solihull = graph.getNode("Solihull");
+            solihull.setAttribute("xy", 3, -3);
+
+            Node sellyoak = graph.getNode("Sellyoak");
+            sellyoak.setAttribute("xy", -2, -2);
+
+            Node redditch = graph.getNode("Redditch");
+            redditch.setAttribute("xy", -3, -3);
+
+            Node erdington = graph.getNode("Erdington");
+            erdington.setAttribute("xy", 2, 2);
+            
+            Node lichfield = graph.getNode("Lichfield");
+            lichfield.setAttribute("xy", 3, 3);           
+  
+            Node perrybarr = graph.getNode("Perrybarr");
+            perrybarr.setAttribute("xy", -2, 2);               
+            
+            Node walsall = graph.getNode("Walsall");
+            walsall.setAttribute("xy", -3, 3);               
+            
+            
             for (Node node : graph) {
                 node.addAttribute("ui.label", node.getId());
             }
@@ -78,8 +109,11 @@
         protected String styleSheet =
             "node {" +
             "	fill-color: black;" +
+            "   text-alignment: at-right;" +
             "}" +
             "node.marked {" +
-            "	fill-color: red;" +
-            "}";
+            "	fill-color: blue;" +
+            "}" 
+    
+                ;
     }
